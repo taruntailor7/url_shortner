1601213c500e3eb5afd19a7afb715ccd46d5e035
Tarun Tailor <tarun@crimecheck.in>
2024-02-28 23:55:54 +0530

    JWT added

--- controllers/user.js
+++ controllers/user.js
@@ -21,9 +21,11 @@ async function handleUserLogin(req, res) {
       error: "Invalid Username or Password",
     });
 
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+  // const sessionId = uuidv4();
+  // setUser(sessionId, user);
+
+  const token = setUser(user)
+  res.cookie("uid", token);
   return res.redirect("/");
 }
 
